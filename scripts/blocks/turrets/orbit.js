c717adea70633df5fd90d1c0286a6d3b551b3149
pankiwi <arroliga.andrz1@gmail.com>
2021-03-11 07:26:34 -0600

    Fix error :D

--- scripts/blocks/turrets/orbit.js
+++ scripts/blocks/turrets/orbit.js
@@ -5,9 +5,7 @@ const dlib = require("infiniteprogram/libs/drawlib");
 //Effect
 
 const orbEffect = new Effect(25, e => {
-    if(Mathf.chance(0.02)){
-	dlib.splashlineii(e.x, e.y, Color.valueOf("abdbff"), Color.valueOf("f0f8ff"), e.fin(), e.fout() * 4.725, e.fslope() * 22 + 20, e.id, 1, 1 + 0 * e.fin(), e.rotation, 0);
-	}
+    dlib.splashlineii(e.x, e.y, Color.valueOf("abdbff"), Color.valueOf("f0f8ff"), e.fin(), e.fout() * 4.725, e.fslope() * 22 + 20, e.id, 1, 1 + 0 * e.fin(), e.rotation, 0);
 });
 
 const chargeEffect = elib.swillEffect(30, Color.valueOf("abdbff"), Color.valueOf("f0f8ff"), 1, 6, 5, 3, 1, 20, 120);
@@ -21,20 +19,21 @@ const chargeEffectBegin = new Effect(60, e => {
 //Bullet
 const orbBullet = extend(MissileBulletType, {
   lifetime : 95,
-  despawnEffect : Fx.hitLancer,
-  hitEffect : Fx.hitLancer,
+  despawnEffect : Fx.none,
+  hitEffect : Fx.none,
   speed : 7.125,
   damage : 50,
   shootEffect : Fx.lightningShoot,
   pierce : false,
   update(b) {
-    const target = Units.bestTarget(b.team, b.x, b.y,600, e => !b.dead && (b.isGrounded() && true) || (b.isFlying() && true), b => true, (u, x, y) => Mathf.dst2(x, y, u.x, u.y));
+    var target = Units.bestTarget(b.team, b.x, b.y, 600, e => !e.dead && (e.isGrounded() && true) || (e.isFlying() && true), b => true, (u, x, y) => Mathf.dst2(x, y, u.x, u.y));
     if (target != null) {
       b.vel.setAngle(Mathf.slerpDelta(b.rotation(), b.angleTo(target), 0.6));
     };
-    if (b.timer.get(1, 0.00001)) {
-      orbEffect.at(b.x, b.y, b.rotation(), Color.valueOf("C2FF8300"));
+        if (b.timer.get(1, 0.00001)) {
+      orbEffect.at(b.x, b.y - 5, b.rotation(), Color.valueOf("C2FF8300"));
     };
+    
   },
   draw(b) {}
 });
